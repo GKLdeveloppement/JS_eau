1214ab4403b46776de8f0732e346926d66d541a4
Semih Gokol <gokol.semih@gmail.com>
2022-11-19 11:16:52 +0100

    exo done

--- eau09.js
+++ eau09.js
@@ -20,21 +20,53 @@ Afficher error et quitter le programme en cas de problèmes d’arguments.
 */
 
 //Initialisation des variables
-let arg1 = process.argv[2]
-let arg2 = process.argv[3]
+let arg1 = parseInt(process.argv[2])
+let arg2 = parseInt(process.argv[3])
+let argUnvalaible = process.argv[4]
 let res = ""
+let suite = 0
+let diff = 0
+
 
 //f() utilisées
+function between2nb(){
+    
+    if (arg1 < arg2) {
+        //arg2 + grand
+        diff = arg2 - arg1;
 
+        for (let i = 0; i < diff; i++) {
+            suite = arg1+i
+            res += suite + " "
+        }
+        
+        return res
+    } else {
+        //arg1 + grand
+        diff = arg1 - arg2;
+
+        for (let i = 0; i < diff; i++) {
+            suite = arg2+i
+            res += suite + " "
+        }
+        
+        return res
+    }
+}
 
 //Gestion d'erreurs
 if (arg1 == undefined || arg2 == undefined || argUnvalaible !== undefined) {
-    console.log("Merci d'entrer 2 arguments valables");
+    console.log("Merci d'entrer que 2 arguments valables");
     return
 }
 
-if(!isNaN(arg1) || !isNaN(arg2)) {
-    console.log("Merci d'entrer une valeur valable");
+if(isNaN(arg1) || isNaN(arg2)) {
+    console.log("Merci d'entrer que des chiffres");
+    return
+}
+
+if (arg1 == arg2) {
+    console.log("Aucune suite n'apparaît car les 2 sont égaux.");
     return
 }
 
@@ -42,7 +74,7 @@ if(!isNaN(arg1) || !isNaN(arg2)) {
 
 
 //Traitement
-let result = ()
+let result = between2nb()
 
 //Affichage résultat
 console.log(result);
