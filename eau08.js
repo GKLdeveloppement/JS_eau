6601c25f71ce61cefaf0eafe92807fb3134beed2
Semih Gokol <gokol.semih@gmail.com>
2022-11-19 10:30:03 +0100

    exercice done

--- eau08.js
+++ eau08.js
@@ -2,11 +2,9 @@
 
 Créez un programme qui détermine si une chaîne de caractères ne contient que des chiffres.
 
-
 Exemples d’utilisation :
 $> python exo.py “4445353”
 true
-
 
 $> python exo.py 42
 true
@@ -15,25 +13,27 @@ $> python exo.py “Bonjour 36”
 false
 
 Afficher error et quitter le programme en cas de problèmes d’arguments.
-
 */
 
 //Initialisation des variables
 let arg1 = process.argv[2]
 let arg2 = process.argv[3]
-let res = ""
+let res = 0
+let nbRegex = /^\d+$/
 
 //f() utilisées
-
-
-//Gestion d'erreurs
-if (arg1 == undefined || arg2 == undefined || argUnvalaible !== undefined) {
-    console.log("Merci d'entrer 2 arguments valables");
-    return
+function isNumber(){
+    if (arg1.match(nbRegex)) {
+        res = true
+        return res
+    } else {
+        return false
+    }
 }
 
-if(!isNaN(arg1) || !isNaN(arg2)) {
-    console.log("Merci d'entrer une valeur valable");
+//Gestion d'erreurs
+if (arg1 == undefined || arg2 !== undefined) {
+    console.log("Merci d'entrer qu'un arguments valables");
     return
 }
 
@@ -41,7 +41,7 @@ if(!isNaN(arg1) || !isNaN(arg2)) {
 
 
 //Traitement
-let result = ()
+let result = isNumber()
 
 //Affichage résultat
 console.log(result);
