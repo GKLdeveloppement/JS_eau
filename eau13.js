c5635a5ab82ee32f9a6eeb103896caaac6784a99
Semih Gokol <gokol.semih@gmail.com>
2022-11-22 16:20:52 +0100

    exo tri selectif done

--- eau13.js
+++ eau13.js
@@ -25,21 +25,53 @@ Wikipedia vous présentera une belle description de cet algorithme de tri.
 */
 
 //Initialisation des variables
+let args = process.argv.slice(2)
 let arg1 = process.argv[2]
 let arg2 = process.argv[3]
-let res = ""
+let arg3 = process.argv[4]
+let res = false
+const nbRegex = /^-?[0-9]\d*(\.\d+)?$/
 
 //f() utilisées
+function isNumber(args){
+    let counter = 0
+    for (let i = 0; i < args.length; i++) {
+        if (args[i].match(nbRegex)) {
+            res = true
+        } else {
+            counter ++
+        }        
+    }
+    return counter
+}
 
+//Main f()
+function selectSort(arr) {
+    let arrSort = [...arr]
+    for (let i = 0; i < (arrSort.length -1); i++) {
+        let min = i
+        for (let j = i+1; j < (arrSort.length); j++) {
+            if(parseInt(arrSort[j]) < parseInt(arrSort[min])) {
+                min = j
+            }
+        }
+        if (min !== i) {
+            let temp = arrSort[i]
+            arrSort[i] = arrSort[min]
+            arrSort[min]= temp
+        }
+    }
+    return arrSort
+}
 
 //Gestion d'erreurs
-if (arg1 == undefined || arg2 == undefined || argUnvalaible !== undefined) {
-    console.log("Merci d'entrer 2 arguments valables");
+if (arg1 == undefined || arg2 == undefined || arg3 == undefined) {
+    console.log("Merci d'entrer 3 arguments valables au minimum");
     return
 }
 
-if(!isNaN(arg1) || !isNaN(arg2)) {
-    console.log("Merci d'entrer une valeur valable");
+if (isNumber(args) > 0) {
+    console.log("Ce script ne peut prendre que des chiffres en arguments");
     return
 }
 
@@ -47,7 +79,7 @@ if(!isNaN(arg1) || !isNaN(arg2)) {
 
 
 //Traitement
-let result = ()
+let result = selectSort(args).join(" ")
 
 //Affichage résultat
 console.log(result);
